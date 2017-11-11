aed1cd29eff43b05ef0e0ca8d90d1e149ad5b85f
Marc Breitung <mbreitung@drole.de>
2017-11-11 15:42:37 +0100

    added distance(point) method to point

--- test/Unit/Map/PoinTest.js
+++ test/Unit/Map/PoinTest.js
@@ -5,13 +5,19 @@ import {Point} from './../../../lib/Map/Point';
 suite('Point', function () {
 
     suite('#constructor(x, y)', function () {
-
         test('should set x, y attributes', function () {
             let point = new Point(10, 20);
             assert.propertyVal(point, 'x', 10);
             assert.propertyVal(point, 'y', 20);
         });
+    });
 
+    suite('#distance(point)', function () {
+        test('should return distance between to points', function () {
+            let pointA = new Point(10, 20);
+            let pointB = new Point(50, 40);
+            assert.closeTo(pointA.distance(pointB), 44.72135955, 0.5);
+        });
     });
 
 });
