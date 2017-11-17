3085634876e8e6bdf6c36ee015563cdacc4338ab
Marc Breitung <mbreitung@drole.de>
2017-11-17 07:48:00 +0100

    added config for node module

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
-module.exports = {
+module.exports = [{
     entry: require.resolve('./lib/problem-map-generator.js'),
     output: {
         path: path.resolve(__dirname, 'dist'),
@@ -29,5 +29,33 @@ module.exports = {
     },
     plugins: [
         new UglifyJSPlugin()
+    ],
+    entry: require.resolve('./lib/problem-map-generator.js'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'problem-map-generator.node.min.js',
+        libraryTarget: 'commonjs'
+    },
+    module: {
+        rules: [
+            {
+                enforce: 'pre',
+                test: /\.js$/,
+                include: [path.resolve(__dirname, 'lib')],
+                exclude: [path.resolve(__dirname, 'node_modules')]
+            },
+            {
+                test: /\.js$/,
+                include: [path.resolve(__dirname, 'lib')],
+                exclude: [path.resolve(__dirname, 'node_modules')],
+                loader: 'babel-loader',
+                options: {
+                    presets: ['env']
+                }
+            }
+        ]
+    },
+    plugins: [
+        new UglifyJSPlugin()
     ]
-};
+}]
